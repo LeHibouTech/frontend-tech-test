6a5296eb202678e92684a4d28c8071a60f966a09
damevin <r.damevin@lehibou.com>
2024-06-12 17:22:32 +0400

    feat: :lipstick: create header component

---
+++ components/UiHeader.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="bg-neutral-700 px-5 py-4">
+		<div class="flex flex-row justify-between">
+			<div class="flex items-center gap-2">
+				<Icon
+					class="text-base"
+					name="ph:shopping-cart-simple-bold"
+				/>
+				<h1 class="text-base">
+					Shopping List App
+				</h1>
+			</div>
+			<UButton>Create new list</UButton>
+		</div>
+	</header>
+</template>
