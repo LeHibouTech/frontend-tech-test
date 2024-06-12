f3e8f247f92a266c7bd34461074485b37f158e95
damevin <r.damevin@lehibou.com>
2024-06-12 16:59:46 +0400

    chore: :wrench: set runtimeConfig, add nuxt/ui and set eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { 
-    enabled: true 
-  },
-  modules: [
-    "@nuxtjs/tailwindcss", 
-    "@nuxt/eslint"
-  ],
-  eslint: {
-    config: {
-      stylistic: {
-        indent: 'tab',
-        semi: true,
-        quotes: 'single',
-      }
-    }
-  }
-})
+	devtools: {
+		enabled: true,
+	},
+	modules: ['@nuxt/eslint', '@nuxt/ui'],
+	runtimeConfig: {
+		apiAppKeySecret: '',
+		apiAppIdSecret: '',
+	},
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+				semi: true,
+				quotes: 'single',
+			},
+		},
+	},
+});
